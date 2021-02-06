488c1007d62e9d81704aaac7ecc95bfe37457a73
Matthew Molloy <whamtet@gmail.com>
2021-02-06 22:21:10 +0700

    geosearch

--- renderer.js
+++ renderer.js
@@ -2,3 +2,5 @@ var map = L.map('map').setView([13.7563, 100.5018], 12);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+L.Control.geocoder().addTo(map);
